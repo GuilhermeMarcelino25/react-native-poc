37c0356deaeeeb711e2e84e1ea7a242c0b505f0d
Guilherme <guilherme.silva.ext@sascar.com.br>
2019-11-16 19:36:56 -0300

    evolucao da demanda

--- component/Post.js
+++ component/Post.js
@@ -6,23 +6,64 @@ import {
   View,
   Image,
   Dimensions,
-  FlatList
+  FlatList,
+  TouchableOpacity
 } from 'react-native';
+
 
 const width = Dimensions.get('screen').width;
 
 export default class Post extends Component {
 
-    render() {
+  constructor(props){
+    super(props);
+    this.state = {
+      foto: {...this.props.foto,likers:[{},{}]}
+    } 
+  }
 
+  mostrarIcone(likeada){
+    return likeada ? require('../resource/img/s2-checked.png') : require('../resource/img/s2.png');
+  }
+
+  interagirFoto(){
+    const fotoAtualizada = {
+      ...this.state.foto,
+      likeada: !this.state.foto.likeada
+    }
+
+    this.setState({foto: fotoAtualizada})
+  }
+
+  mostrarLikers(likers){
+
+    if(likers.length <= 0)
+    return;
+
+    return (
+      <Text>{likers.length} {likers.length > 1 ? 'curtidas' : 'curtida'}</Text>
+    );
+  }
+
+    render() {
+      const {foto} = this.state;
 
      return (
                 <View>
                 <View style= {styles.cabecalho}>  
-                <Image source={{uri: this.props.foto.urlPerfil}} style={styles.fotoDePerfil} />
-                <Text>{this.props.foto.loginUsuario}</Text>
+                <Image source={{uri: foto.urlPerfil}} style={styles.fotoDePerfil} />
+                <Text>{foto.loginUsuario}</Text>
                 </View>    
-                <Image source={{uri: this.props.foto.urlFoto}}  style={styles.foto} />
+                <Image source={{uri: foto.urlFoto}}  style={styles.foto} />
+                
+                <View style= {styles.rodape}>
+                <TouchableOpacity onPress={this.interagirFoto.bind(this)}>
+                <Image style= {styles.botaoDeLike} 
+                    source={this.mostrarIcone(foto.likeada)}/>
+                </TouchableOpacity>
+                {this.mostrarLikers(foto.likers)}  
+                </View>
+                             
                 </View>
         );
     }
@@ -47,6 +88,16 @@ const styles = StyleSheet.create({
     foto:{
       width:width, 
       height:width
+    },
+
+    botaoDeLike:{
+      marginBottom:10,
+      width:40,
+      height:40
+    },
+
+    rodape:{
+      margin: 10
     }
   
   });
