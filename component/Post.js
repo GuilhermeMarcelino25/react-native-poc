d46abb82d7a2db10efd4a58fb32d27b645015251
gmspgua <guilhermemarcelinodasilva@hotmail.com>
2019-11-18 19:42:32 -0300

    adicao funcao like dinamico

--- component/Post.js
+++ component/Post.js
@@ -18,7 +18,7 @@ export default class Post extends Component {
   constructor(props){
     super(props);
     this.state = {
-      foto: {...this.props.foto,likers:[{},{}]}
+      foto: {...this.props.foto, likers: [{login: 'Joao'}]}
     } 
   }
 
@@ -26,23 +26,50 @@ export default class Post extends Component {
     return likeada ? require('../resource/img/s2-checked.png') : require('../resource/img/s2.png');
   }
 
+
   interagirFoto(){
+    
+
+    let novaLista = [];
+
+     if(!this.state.foto.likeada){
+      novaLista = [...this.state.foto.likers , {login: 'Guilherme'}];
+    } else{
+      novaLista = this.state.foto.likers.filter(liker => {
+        return liker.login !== 'Guilherme';
+      })
+    } 
+ 
     const fotoAtualizada = {
       ...this.state.foto,
-      likeada: !this.state.foto.likeada
+      likeada: !this.state.foto.likeada,
+      likers: novaLista,
     }
-
     this.setState({foto: fotoAtualizada})
+   
   }
 
   mostrarLikers(likers){
-
     if(likers.length <= 0)
     return;
 
     return (
-      <Text>{likers.length} {likers.length > 1 ? 'curtidas' : 'curtida'}</Text>
+      <Text style= {styles.like} >{likers.length} {likers.length > 1 ? 'curtidas' : 'curtida'}</Text>
     );
+  }
+
+  exibeLegenda(foto){
+    if(foto.comentario === ''){
+      return;
+    }
+
+    return(
+                      <View style= {styles.comentario}>
+                          <Text style={styles.tituloComentario}>{foto.loginUsuario}</Text>  
+                          <Text>{foto.comentario}</Text>
+                        </View>
+    );
+
   }
 
     render() {
@@ -50,20 +77,20 @@ export default class Post extends Component {
 
      return (
                 <View>
-                <View style= {styles.cabecalho}>  
-                <Image source={{uri: foto.urlPerfil}} style={styles.fotoDePerfil} />
-                <Text>{foto.loginUsuario}</Text>
-                </View>    
-                <Image source={{uri: foto.urlFoto}}  style={styles.foto} />
-                
-                <View style= {styles.rodape}>
-                <TouchableOpacity onPress={this.interagirFoto.bind(this)}>
-                <Image style= {styles.botaoDeLike} 
-                    source={this.mostrarIcone(foto.likeada)}/>
-                </TouchableOpacity>
-                {this.mostrarLikers(foto.likers)}  
-                </View>
-                             
+                    <View style= {styles.cabecalho}>  
+                      <Image source={{uri: foto.urlPerfil}} style={styles.fotoDePerfil} />
+                      <Text>{foto.loginUsuario}</Text>
+                    </View>    
+                    <Image source={{uri: foto.urlFoto}}  style={styles.foto} />                   
+                    <View style= {styles.rodape}>
+                      <TouchableOpacity onPress={this.interagirFoto.bind(this)}>
+                        <Image style= {styles.botaoDeLike} 
+                            source={this.mostrarIcone(foto.likeada)}/>
+                      </TouchableOpacity>
+                      {this.mostrarLikers(foto.likers)}
+                      {this.exibeLegenda(foto)}
+                        
+                      </View>                            
                 </View>
         );
     }
@@ -95,9 +122,20 @@ const styles = StyleSheet.create({
       width:40,
       height:40
     },
-
     rodape:{
       margin: 10
+    },
+    like:{
+      fontWeight: 'bold'
+    },
+
+    comentario: {
+      flexDirection: 'row'
+    },
+
+    tituloComentario :{
+      fontWeight: 'bold',
+      marginRight: 5
     }
   
   });
